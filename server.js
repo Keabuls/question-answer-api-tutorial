4eda75b544ad9b1ded05be719d3a54289f0c03cf
Keabuls <asonmezkaan@hotmail.com>
2023-09-11 13:08:49 +0300

    Router'ın İlk kısmı bitti

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express")
+const dotenv = require("dotenv")
+const questions = require("./routers/question")
+const auth = require("./routers/auth")
+
+
+// Enviroment Variables
+dotenv.config({
+    path:"./config/env/config.env"
+})
+
+// localhost:5000/api/questions
+// localhost:5000/api/auth
 
 const app = express()
+const PORT = process.env.PORT;
 
-const PORT = 5000 || process.env.PORT;
+// Routers Middleware
+
+app.use("/api/questions",questions)
+app.use("/api/auth",auth)
+
+
+
 
 app.listen(PORT,()=>{
-    console.log(`Àpp Started on ${PORT}`)
+    console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`)
 })
 
 
